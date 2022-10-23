fef6d5688f1da9880dd409718556bf6a42d37b83
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-23 20:56:33 +0300

    Style Homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Characters from '../components/Characters'
-import { Navbar, Container } from 'react-bootstrap'
+import Filters from '../components/Filters'
+import { Navbar, Container, Button } from 'react-bootstrap'
+import { FaFilter } from 'react-icons/fa'
+import { useGlobalContext } from '../context/context'
 
 const HomePage = () => {
+  const { setShowModal, showModal } = useGlobalContext()
+  const handleOpen = () => {
+    console.log(showModal.filter)
+    setShowModal((prev) => ({ ...prev, show: true }))
+  }
   return (
     <div>
       {' '}
@@ -20,6 +28,23 @@ const HomePage = () => {
           </Navbar.Brand>
         </Container>
       </Navbar>
+      <Container className='my-3 py-3 d-flex align-items-center rounded'>
+        <h3 style={{ display: 'inline', color: 'dark' }} className='my-2 mx-2'>
+          {showModal.filter === ''
+            ? 'Rick And Morty'
+            : showModal.filter === 'rick'
+            ? 'Rick'
+            : 'Morty'}
+        </h3>
+        <Button
+          onClick={handleOpen}
+          variant='outline-light'
+          className='btn-dark'
+        >
+          <FaFilter />
+        </Button>
+      </Container>
+      <Filters />
       <Characters />
     </div>
   )
