d3f4d2998cc7f7167443db0a803c20012f230b9e
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-23 20:25:11 +0300

    Fix infinite scrolling

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,50 +1,68 @@
 import React, { useState, useEffect } from 'react'
-import { useQuery } from '@apollo/client'
+import { useQuery, NetworkStatus } from '@apollo/client'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
 import { GET_CHARACTERS_QUERY } from '../graphql/queries'
 import { Character, CharacterType } from './Character'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
+import { useGlobalContext } from '../context/context'
 
 const Characters = () => {
+  const { showModal } = useGlobalContext()
   const [page, setPage] = useState<number>(1)
-  const [items, setItems] = useState<CharacterType[] | []>([])
-  const { data, loading } = useQuery(GET_CHARACTERS_QUERY, {
-    variables: { page: page },
+  const { data, loading, error, fetchMore } = useQuery(GET_CHARACTERS_QUERY, {
+    variables: { page: page, name: showModal.filter },
   })
 
-  useEffect(() => {
-    if (data && data.characters.results) {
-      setItems((prevState) => [...prevState, ...data.characters.results])
-    }
-  }, [data])
-
   const hasMore = () => {
-    if (data) {
-      console.log(data.characters.info.pages)
-    }
-    return data && page < data.characters.info.pages
+    fetchMore({
+      variables: {
+        page: data.characters.info.next,
+      },
+      updateQuery: (prevPage, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          return prevPage
+        }
+        return {
+          characters: {
+            __typename: 'Characters',
+            info: {
+              __typename: 'Info',
+              ...prevPage.characters.info,
+              ...fetchMoreResult.characters.info,
+            },
+            results: [
+              ...prevPage.characters.results,
+              ...fetchMoreResult.characters.results,
+            ],
+          },
+        }
+      },
+    })
   }
 
+  if (loading) return <h1>loading</h1>
+  if (error) return <h1>error</h1>
   return (
     <Container>
-      {items && items.length > 0 && (
-        <InfiniteScroll
-          dataLength={items.length}
-          next={() => setPage(page + 1)}
-          hasMore={hasMore()}
-          loader={<h4>Loading...</h4>}
-        >
-          <Row>
-            {items.map((elem: CharacterType) => (
-              <Character character={elem} />
-            ))}
-          </Row>
-        </InfiniteScroll>
-      )}
-      {items && items.length === 0 && !loading && (
-        <span className='messageNoResults'>Ups !! Try again !!</span>
-      )}
+      <InfiniteScroll
+        dataLength={data.characters.results.length}
+        next={hasMore}
+        hasMore={page < data.characters.info.pages}
+        loader={<h4>Loading...</h4>}
+      >
+        <Row>
+          {data.characters.results.map((elem: CharacterType) => (
+            <Character character={elem} />
+          ))}
+        </Row>
+      </InfiniteScroll>
+
+      {data.characters.results &&
+        data.characters.results.length === 0 &&
+        !loading && (
+          <span className='messageNoResults'>Ups !! Try again !!</span>
+        )}
     </Container>
   )
 }
